638739f81f182600d85c060514a493a0a4cee51a
Abdul Syed <syed_abdul_basit@hotmail.com>
2022-09-21 11:48:22 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,21 @@
-def buildApp () {
-    echo 'building the application ...'
+def buildApp() {
+    echo 'building the application'
+    sh 'mvn package'
 }
 
-def testApp() {
-    echo "testing the application ..."
+def buildImage() {
+    echo 'building the docker image'
+    withCredentials([usernamePassword(
+        credentialsId: '71ef5aa5-53bc-40e9-bdf6-4cfd27c1aa7e',
+        passwordVariable: 'PASS',
+        usernameVariable: 'USER'
+        )]) {
+            sh 'docker build -t abz7896/prod-repo:jma-2.0 .'
+            sh "echo $PASS | docker login -u $USER --password-stdin"
+            sh 'docker push abz7896/prod-repo:jma-2.0'
+        }
 }
 
 def deployApp() {
-    echo "deploying the application ..."
-    echo "deploying version ${params.VERSION}"
-}  
-
-return this
+    echo 'deploying the application'
+}
